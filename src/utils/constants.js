d108db4fb3a3452ab79aa068cafd5ae4175a1612
Rafaela Leão <rleao2903@gmail.com>
2025-07-26 01:58:24 -0300

    Delete src directory

--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const PATH_SPACESHIP_IMAGE = "./src/assets/images/spaceship.png";
-export const PATH_ENGINE_IMAGE = "./src/assets/images/engine.png";
-export const PATH_ENGINE_SPRITES_IMAGE = "./src/assets/images/engine_sprites.png";
-export const INITIAL_FRAMES_COUNTER = 8;
